d7e1df754111e7f39cff65d8516a1745910dd024
Shaurya Pratap Singh <shauryajt3455@gmail.com>
2024-07-30 22:50:39 +0530

    page css (Bg color, reset button, h1 tag)

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './TicTacToe.css'
+import circleIcon from '../Assets/circle.png'
+import crossIcon from '../Assets/cross.png'
 
 const TicTacToe = () => {
   return (
-    <div>
-      
+    <div className='container'>
+        <h1 className="title">Tic Tac Toe Game In <span> React</span></h1>
+        <div className="board">
+
+        </div>
+        <button className='reset'>Reset</button>
     </div>
   )
 }
